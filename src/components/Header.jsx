99b89158217e4194527a0ab500274c3169e3af48
Ashfaqul Awal <40667062+awalashfaqul@users.noreply.github.com>
2024-06-14 17:43:00 +0200

    click event fixed for responsive menu on small screen.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import { NavLink, useLocation } from 'react-router-dom';
 import './Header.css';
 
 function Header() {
-    const location = useLocation();
+    //const location = useLocation();
     const [isDropdownOpen, setDropdownOpen] = useState(false);
 
     // const isActive = (path) => location.pathname === path;
@@ -21,7 +21,7 @@ function Header() {
                     <NavLink to="/portfolio" className={({ isActive }) => (isActive ? 'active-link' : '')}>My Portfolio</NavLink>
                     <NavLink to="/contact" className={({ isActive }) => (isActive ? 'active-link' : '')}>Contact Me</NavLink>
                 </div>
-                <a href="javascript:void(0);" className="icon" onClick={toggleDropdown}>
+                <a href="javascript:void(0);" className="icon" onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
                     <i className="fa fa-bars"></i>
                 </a>
             </nav>
